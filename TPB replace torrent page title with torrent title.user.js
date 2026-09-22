cbb05d96b8f08ec17547e2ef0b861e8a0f88abc8
Mike Gerber <mike@mike-gerber.de>
2026-09-22 23:26:52 +0200

    🐛 Fix typo: I seem to be unable to write 'violent'

--- TPB replace torrent page title with torrent title.user.js
+++ TPB replace torrent page title with torrent title.user.js
@@ -2,7 +2,7 @@
 // @name        TPB replace torrent page title with torrent title
 // @namespace   https://github.com/mikegerber
 // @version     0.0.1
-// @downloadURL https://raw.githubusercontent.com/mikegerber/violetmonkey-userscripts/master/TPB%20replace%20torrent%20page%20title%20with%20torrent%20title.user.js
+// @downloadURL https://raw.githubusercontent.com/mikegerber/violentmonkey-userscripts/master/TPB%20replace%20torrent%20page%20title%20with%20torrent%20title.user.js
 // @match       https://thepiratebay.org/description.php*
 // @grant       none
 // @run-at     document-end
