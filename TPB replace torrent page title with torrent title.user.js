55f75fc5cbb19aaa834fca1c38a3dedb9cf8cbe3
Mike Gerber <mike@mike-gerber.de>
2026-09-22 23:32:25 +0200

    🚧 TPB replace: Use camelCase, also check for nulls

--- TPB replace torrent page title with torrent title.user.js
+++ TPB replace torrent page title with torrent title.user.js
@@ -2,7 +2,7 @@
 // @name        TPB replace torrent page title with torrent title
 // @namespace   https://github.com/mikegerber
 // @description Replace TPB's generic page title with the torrent title.
-// @version     0.0.1
+// @version     0.0.2
 // @downloadURL https://raw.githubusercontent.com/mikegerber/violentmonkey-userscripts/master/TPB%20replace%20torrent%20page%20title%20with%20torrent%20title.user.js
 // @match       https://thepiratebay.org/description.php*
 // @grant       none
@@ -11,7 +11,9 @@
 // ==/UserScript==
 
 
-const label_name = document.querySelector("label#name").innerText;
-const new_title = `TPB: ${label_name}`;
+const labelName = document.querySelector("label#name")?.textContent?.trim();
 
-document.title = new_title;
+if (labelName) {
+  const newTitle = `TPB: ${labelName}`;
+  document.title = newTitle;
+}
